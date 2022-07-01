c8da1573a2677e103b953bfd9f6966471b517f2e
Stepan Rudenko <s.rudenko584@gmail.com>
2022-07-01 23:59:21 +0300

    added Skeleton import

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from "./components/Header";
 import Categories from "./components/Categories";
 import Sort from "./components/Sort";
 import PizzaBlock from "./components/PizzaBlock";
+import Skeleton from "./components/SkeletonforPizzaBlock";
 
 import "./scss/app.scss";
 
