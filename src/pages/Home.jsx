28c5eb5fbc60ee7ab2c714bd55509172e9f108fe
Stepan Rudenko <s.rudenko584@gmail.com>
2022-07-02 21:17:21 +0300

    Home and NotFound pages moved to separate files

---
+++ src/pages/Home.jsx
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from "react";
+
+import Categories from "../components/Categories";
+import Sort from "../components/Sort";
+import PizzaBlock from "../components/PizzaBlock";
+import Skeleton from "../components/SkeletonforPizzaBlock";
+
+const Home = () => {
+  const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("https://62beda5fbe8ba3a10d5d3961.mockapi.io/items")
+      .then((res) => res.json())
+      .then((arr) => {
+        setItems(arr);
+        setIsLoading(false);
+      });
+  }, []);
+
+  return (
+    <>
+      <div className="content__top">
+        <Categories />
+        <Sort />
+      </div>
+      <h2 className="content__title">Все пиццы</h2>
+      <div className="content__items">
+        {isLoading
+          ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
+          : items.map((obj) => <PizzaBlock key={obj.id} {...obj} />)}
+      </div>
+    </>
+  );
+};
+
+export default Home;
