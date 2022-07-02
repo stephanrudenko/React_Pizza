f090e2a3b2be113a20dc972afbd5f79ea093f743
Stepan Rudenko <s.rudenko584@gmail.com>
2022-07-02 21:10:26 +0300

    added routes for Home, NotFound  page and moved this functions to dedicated files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 
 import Header from "./components/Header";
-import Categories from "./components/Categories";
-import Sort from "./components/Sort";
-import PizzaBlock from "./components/PizzaBlock";
-import Skeleton from "./components/SkeletonforPizzaBlock";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
 
 import "./scss/app.scss";
 
@@ -14,16 +13,10 @@ function App() {
       <Header />
       <div className="content">
         <div className="container">
-          <div className="content__top">
-            <Categories />
-            <Sort />
-          </div>
-          <h2 className="content__title">Все пиццы</h2>
-          <div className="content__items">
-            {isLoading
-              ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
-              : items.map((obj) => <PizzaBlock key={obj.id} {...obj} />)}
-          </div>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/not-found" element={<NotFound />} />
+          </Routes>
         </div>
       </div>
     </div>
